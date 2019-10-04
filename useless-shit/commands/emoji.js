17edf8948ed29bb6d7cf808e4da0f7043b2c909c
Ryan <ryrylin@gmail.com>
2019-10-04 17:55:49 +0800

    .gitignore fix

--- useless-shit/commands/emoji.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: "emoji",
-	description: "Returns info of an emoji.",
-	aliases: ["e", "emote"],
-	execute(message, args){
-		message.channel.send("This command is currently under development! Sorry!");
-	},
-};
